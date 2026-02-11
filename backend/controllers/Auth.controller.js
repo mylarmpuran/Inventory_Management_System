8004825910e7317cf2d392652574f9a3c791da92
mylarmpuran <mylarmpuran@gmail.com>
2026-02-11 14:10:18 +0530

    "authentications"

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -5,10 +5,20 @@ import AuthService from "../services/Auth.services"
 
 
 class AuthController{
-    static RegisterUser = CatchAsync(async(req,res) => {
-        const res_obj = await AuthService.RegisterUser(req.body);
-        res.status(httpStatus.CREATED).send(res_obj)
-    })
+        static RegisterUser = CatchAsync(async(req,res)=>{
+            const res_obj = await AuthService.RegisterUser(req.body);
+            res.status(httpStatus.CREATED).send(res_obj)
+        }) 
+    static LoginUser = CatchAsync(async(req,res)=>{
+            const res_obj = await AuthService.LoginUser(req.body);
+            res.status(httpStatus.OK).send(res_obj)
+        })
+          static ProfileController = CatchAsync(async(req,res)=>{
+            const res_obj = await AuthService.ProfileService(req.user);
+            res.status(httpStatus.OK).send(res_obj)
+        })
+         
+        
 }
 
 export default AuthController
---
+++ backend/controllers/authorization/jwt.token.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const createToken = (userId) => {
+    return jwt.sign(
+        {id:userId},
+        process.env.JWT_SECRET,
+        {expiresIn: '7d'}
+    );
+};
+
+
+
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;      // so you can use req.user.id in routes
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
